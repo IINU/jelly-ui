e92d87ee5e51662588ffe3796e302a457cf7f21c
Jack Price-Burns <jackpriceburns@outlook.com>
2024-06-17 15:47:09 +0100

    fix: state showcase compile error

--- src/showcase/StateShowcase.tsx
+++ src/showcase/StateShowcase.tsx
@@ -4,11 +4,11 @@ import { AppLayout } from '../layouts/AppLayout'
 type Props = {
   state: ComponentType<{ disabledText?: string, ctaClicked: () => void }>
   disabledText?: string
-  layoutState?: 'cookbook' | 'homescreen'
+  layoutState?: 'homescreen' | 'tabbed' | 'title'
   onClick: (...args: unknown[]) => void
 }
 
-export function StateShowcase({ state: State, disabledText, onClick, layoutState = 'cookbook' }: Props) {
+export function StateShowcase({ state: State, disabledText, onClick, layoutState = 'tabbed' }: Props) {
   return (
     <AppLayout state={layoutState}>
       <div className="min-h-full p-4 flex justify-center">
