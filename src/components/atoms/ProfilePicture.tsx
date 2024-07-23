21bbca3df4cae53e48d9d6839e5ce1083b10a1b9
Jack Price-Burns <jackpriceburns@outlook.com>
2024-07-23 14:04:08 +0100

    fix: profile picture onClick

--- src/components/atoms/ProfilePicture.tsx
+++ src/components/atoms/ProfilePicture.tsx
@@ -1,21 +1,23 @@
+import { MouseEventHandler } from 'react'
+
 type Props = {
   active?: boolean
   src: string
   className?: string
-  onClick?: (url: string) => void
+  onClick?: MouseEventHandler<HTMLDivElement>
 }
 
 export function ProfilePicture({ active = false, src, className, onClick }: Props) {
   const activeClass = active ? 'jui-bg-secondary-400' : 'jui-bg-white'
-  const ppBase = `jui-flex jui-justify-center jui-items-center jui-w-36 jui-h-36 jui-shadow jui-rounded-full jui-p-1 ${activeClass}`
+  const ppBase = `jui-flex jui-justify-center jui-items-center jui-w-36 jui-h-36 jui-shadow jui-rounded-full jui-p-1 jui-cursor-pointer ${activeClass}`
   const imgBase = 'jui-w-full jui-h-full jui-rounded-full jui-object-cover'
 
   return (
-    <button
+    <div
       className={`${ppBase} ${className}`}
-      onClick={() => onClick?.(src)}
+      onClick={onClick}
     >
       <img src={src} alt="Chef" className={imgBase}/>
-    </button>
+    </div>
   )
 }
