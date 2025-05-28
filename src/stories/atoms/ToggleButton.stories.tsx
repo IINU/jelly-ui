26f281d59e1f8b94decd67d23fca4a68b4a336c5
dkjelly <205746948+dkjelly@users.noreply.github.com>
2025-05-28 17:32:48 +0900

    feat: add support for disabled state in ToggleButton component

--- src/stories/atoms/ToggleButton.stories.tsx
+++ src/stories/atoms/ToggleButton.stories.tsx
@@ -1,6 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { ToggleButtonShowcase } from '../../showcase/ToggleButtonShowcase'
-import { fn } from '@storybook/test'
 
 const meta = {
   title: 'Atoms/Toggle Button',
@@ -12,8 +11,5 @@ export default meta
 type Story = StoryObj<typeof meta>;
 
 export const ToggleButton: Story = {
-  args: {
-    onClick: fn(),
-    label: 'Head Chef',
-  },
+  args: {},
 }
