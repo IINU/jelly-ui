4f8901e322a653af99b3992c1f89642a9ac3c26a
robert-jelly <robert@getjelly.co.uk>
2025-07-07 11:15:50 +0100

    JEL-869: Debug async dropdown - Part 3 (#11)

--- src/components/atoms/internal/dropdown/DropdownUI.tsx
+++ src/components/atoms/internal/dropdown/DropdownUI.tsx
@@ -197,7 +197,7 @@ export function DropdownUI<T>({
         )}
       </div>
 
-      {open && !error && createPortal(
+      {open && createPortal(
         <DropdownOptions<T>
           selectedOption={selectedValue}
           options={options}
