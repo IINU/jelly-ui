5c7e07438cf76810cf79c1d10b3e4a39049f8fb4
dkjelly <205746948+dkjelly@users.noreply.github.com>
2026-06-05 18:47:49 +0100

    Add `grey` accent to accentToText utility

--- src/utils/accent.ts
+++ src/utils/accent.ts
@@ -1,11 +1,15 @@
-export type Accent = 'error' | 'success' | 'tertiary' | 'secondary'
+export type Accent = 'error' | 'success' | 'tertiary' | 'secondary' | 'grey'
 
-export function accentToText(accent?: Accent, fallback = 'jui-text-primary-900') {
+export function accentToText(
+  accent?: Accent,
+  fallback = 'jui-text-primary-900',
+) {
   const colours: Record<Accent, string> = {
     error: 'jui-text-error-400',
     secondary: 'jui-text-secondary-400',
     success: 'jui-text-success-400',
-    tertiary: 'jui-text-tertiary-400'
+    tertiary: 'jui-text-tertiary-400',
+    grey: 'jui-text-primary-600',
   }
 
   return accent ? colours[accent] : fallback
